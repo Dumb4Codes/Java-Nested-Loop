9db60f71597136835c8e6493613f0a3b852ea2e4
Youvraj Singh <114943098+Dumb4Codes@users.noreply.github.com>
2022-10-16 14:25:06 +0530

    Add files via upload

---
+++ Z6_NestedLoop.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Z6_NestedLoop {
+
+	public static void main(String[] args) {
+		
+		// nested loop = Loop inside a loop 
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		int rows;
+		int columns;
+		String symbol;
+		
+		System.out.println("Enter # of rows");
+		rows = scanner.nextInt();
+		System.out.println("Enter # of columns");
+		columns = scanner.nextInt();
+		System.out.println("Enter symbol");
+		symbol = scanner.next();
+		
+		for(int i = 1; i<=rows; i++) {
+			System.out.println();
+			for(int j = 1; j<=columns; j++) {
+				System.out.print(symbol);
+			}
+				
+		}
+	}
+
+}
